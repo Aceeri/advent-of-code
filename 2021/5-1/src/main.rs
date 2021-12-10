f7b845240f7df885150ee29aec5cc10a56660e1a
Aceeri <conmcclusk@gmail.com>
2021-12-10 05:19:07 -0800

    Day 5 part 1

---
+++ 2021/5-1/src/main.rs
@@ -0,0 +1,50 @@
+use std::{collections::{HashSet, VecDeque}, env, error::Error, fs};
+
+use regex::Regex;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string("input")?;
+
+    let re = Regex::new(r"(\d+),(\d+)\s*->\s*(\d+),(\d+)").unwrap();
+
+    let mut grid = vec![vec![0u8; 1000]; 1000];
+    for captures in re.captures_iter(&contents) {
+        let start_x: usize = captures.get(1).unwrap().as_str().parse()?;
+        let start_y: usize = captures.get(2).unwrap().as_str().parse()?;
+        let end_x: usize = captures.get(3).unwrap().as_str().parse()?;
+        let end_y: usize = captures.get(4).unwrap().as_str().parse()?;
+
+        if start_x == end_x { // horizontal
+            let x = start_x;
+            let min = start_y.min(end_y);
+            let max = start_y.max(end_y);
+            for y in min..=max {
+                grid[y][x] += 1;
+            }
+
+        } else if start_y == end_y { // vertical
+            let y = start_y;
+            let min = start_x.min(end_x);
+            let max = start_x.max(end_x);
+            for x in min..=max {
+                grid[y][x] += 1;
+            }
+        }
+    }
+
+    let mut overlapped = 0;
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            print!("{}", grid[y][x]);
+            if grid[y][x] > 1 {
+                overlapped += 1;
+            }
+        }
+        println!();
+    }
+
+    println!("{:?}", overlapped);
+
+
+    Ok(())
+}
